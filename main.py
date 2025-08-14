886fc27e59a398778c1bca1c791e083e31ab400f
KickoKicko <kickokicko110@gmail.com>
2025-08-14 11:57:26 +0200

    add rubiks_cube class with redndering

--- main.py
+++ main.py
@@ -1,29 +1,117 @@
 from ursina import *
+from rubiks_cube import rubiks_cube
 
 app = Ursina()
-"""cube = Entity(model='cube',position=(0,0,0),color=color.azure)
-cube = Entity(model='cube',position=(1,1,1),color=color.red)
-cube = Entity(model='cube',position=(-1,-1,-1),color=color.green)
-cube = Entity(model='cube',position=(1,-1,1),color=color.white)
-cube = Entity(model='cube',position=(1,1,-1),color=color.pink)
-cube = Entity(model='cube',position=(-1,1,1))"""
-
 
 cube = Entity()
 
-#cube = Entity(model='cube',color=color.red)
-size = 1
-
+"""cube = Entity()
 faces = [
-    Entity(model='quad', color=color.red,   position=(0,0, size/2),rotation_y=180, parent=cube),   # front
-    #Entity(model='quad', color=color.pink,   position=(1,0, size/2),rotation_y=180, parent=cube),   # front
-    Entity(model='quad', color=color.orange,position=(0,0,-size/2), rotation_y=0, parent=cube), # back
-    Entity(model='quad', color=color.blue,  position=(-size/2,0,0), rotation_y=90,  parent=cube), # left
-    Entity(model='quad', color=color.green, position=( size/2,0,0), rotation_y=-90, parent=cube), # right
-    Entity(model='quad', color=color.white, position=(0, size/2,0), rotation_x=90, parent=cube), # top
-    Entity(model='quad', color=color.yellow,position=(0,-size/2,0), rotation_x=-90,  parent=cube)  # bottom
-]
+    [
+    Entity(model='quad', color=color.red, position=(0,0, 1.55),rotation_y=180, parent=cube),
+    Entity(model='quad', color=color.red, position=(1.05,0, 1.55),rotation_y=180, parent=cube),
+    Entity(model='quad', color=color.red, position=(0,1.05, 1.55),rotation_y=180, parent=cube),
+    Entity(model='quad', color=color.red, position=(1.05,1.05, 1.55),rotation_y=180, parent=cube),
+    Entity(model='quad', color=color.red, position=(-1.05,-1.05, 1.55),rotation_y=180, parent=cube),
+    Entity(model='quad', color=color.red, position=(-1.05,0, 1.55),rotation_y=180, parent=cube),
+    Entity(model='quad', color=color.red, position=(0,-1.05, 1.55),rotation_y=180, parent=cube),
+    Entity(model='quad', color=color.red, position=(1.05,-1.05, 1.55),rotation_y=180, parent=cube),
+    Entity(model='quad', color=color.red, position=(-1.05,1.05, 1.55),rotation_y=180, parent=cube),
 
+    Entity(model='quad', color=color.black, position=(0, 0.525, 1.55),scale_y=0.05,scale_x=3.1,rotation_y=180, parent=cube),
+    Entity(model='quad', color=color.black, position=(0, -0.525, 1.55),scale_y=0.05,scale_x=3.1,rotation_y=180, parent=cube),
+    Entity(model='quad', color=color.black, position=(0.525, 0, 1.55),scale_y=3.1,scale_x=0.05,rotation_y=180, parent=cube),
+    Entity(model='quad', color=color.black, position=(-0.525, 0, 1.55),scale_y=3.1,scale_x=0.05,rotation_y=180, parent=cube)],
+
+
+    [
+    Entity(model='quad', color=color.orange, position=(0,0,-1.55), rotation_y=0, parent=cube),
+    Entity(model='quad', color=color.orange, position=(1.05,0,-1.55), rotation_y=0, parent=cube),
+    Entity(model='quad', color=color.orange, position=(0,1.05,-1.55), rotation_y=0, parent=cube),
+    Entity(model='quad', color=color.orange, position=(1.05,1.05,-1.55), rotation_y=0, parent=cube),
+    Entity(model='quad', color=color.orange, position=(-1.05,0,-1.55), rotation_y=0, parent=cube),
+    Entity(model='quad', color=color.orange, position=(0,-1.05,-1.55), rotation_y=0, parent=cube),
+    Entity(model='quad', color=color.orange, position=(-1.05,-1.05,-1.55), rotation_y=0, parent=cube),
+    Entity(model='quad', color=color.orange, position=(-1.05,1.05,-1.55), rotation_y=0, parent=cube),
+    Entity(model='quad', color=color.orange, position=(1.05,-1.05,-1.55), rotation_y=0, parent=cube),
+
+    Entity(model='quad', color=color.black, position=(0, 0.525, -1.55),scale_y=0.05,scale_x=3.1,rotation_y=0, parent=cube),
+    Entity(model='quad', color=color.black, position=(0, -0.525, -1.55),scale_y=0.05,scale_x=3.1,rotation_y=0, parent=cube),
+    Entity(model='quad', color=color.black, position=(0.525, 0, -1.55),scale_y=3.1,scale_x=0.05,rotation_y=0, parent=cube),
+    Entity(model='quad', color=color.black, position=(-0.525, 0, -1.55),scale_y=3.1,scale_x=0.05,rotation_y=0, parent=cube)],
+
+
+    [
+    Entity(model='quad', color=color.blue, position=(-1.55,0,0), rotation_y=90,  parent=cube),
+    Entity(model='quad', color=color.blue, position=(-1.55,1.05,0), rotation_y=90,  parent=cube),
+    Entity(model='quad', color=color.blue, position=(-1.55,0,1.05), rotation_y=90,  parent=cube),
+    Entity(model='quad', color=color.blue, position=(-1.55,1.05,1.05), rotation_y=90,  parent=cube),
+    Entity(model='quad', color=color.blue, position=(-1.55,-1.05,0), rotation_y=90,  parent=cube),
+    Entity(model='quad', color=color.blue, position=(-1.55,0,-1.05), rotation_y=90,  parent=cube),
+    Entity(model='quad', color=color.blue, position=(-1.55,-1.05,-1.05), rotation_y=90,  parent=cube),
+    Entity(model='quad', color=color.blue, position=(-1.55,1.05,-1.05), rotation_y=90,  parent=cube),
+    Entity(model='quad', color=color.blue, position=(-1.55,-1.05,1.05), rotation_y=90,  parent=cube),
+
+    Entity(model='quad', color=color.black, position=(-1.55, 0.525, 0),scale_y=0.05,scale_x=3.1,rotation_y=90, parent=cube),
+    Entity(model='quad', color=color.black, position=(-1.55, -0.525, 0),scale_y=0.05,scale_x=3.1,rotation_y=90, parent=cube),
+    Entity(model='quad', color=color.black, position=(-1.55, 0, 0.525),scale_y=3.1,scale_x=0.05,rotation_y=90, parent=cube),
+    Entity(model='quad', color=color.black, position=(-1.55, 0, -0.525),scale_y=3.1,scale_x=0.05,rotation_y=90, parent=cube)],
+
+
+    [
+    Entity(model='quad', color=color.green, position=( 1.55,0,0), rotation_y=-90, parent=cube),
+    Entity(model='quad', color=color.green, position=( 1.55,1.05,0), rotation_y=-90, parent=cube),
+    Entity(model='quad', color=color.green, position=( 1.55,0,1.05), rotation_y=-90, parent=cube),
+    Entity(model='quad', color=color.green, position=( 1.55,1.05,1.05), rotation_y=-90, parent=cube),
+    Entity(model='quad', color=color.green, position=( 1.55,-1.05,0), rotation_y=-90, parent=cube),
+    Entity(model='quad', color=color.green, position=( 1.55,0,-1.05), rotation_y=-90, parent=cube),
+    Entity(model='quad', color=color.green, position=( 1.55,-1.05,-1.05), rotation_y=-90, parent=cube),
+    Entity(model='quad', color=color.green, position=( 1.55,1.05,-1.05), rotation_y=-90, parent=cube),
+    Entity(model='quad', color=color.green, position=( 1.55,-1.05,1.05), rotation_y=-90, parent=cube),
+
+    Entity(model='quad', color=color.black, position=(1.55, 0.525, 0),scale_y=0.05,scale_x=3.1,rotation_y=-90, parent=cube),
+    Entity(model='quad', color=color.black, position=(1.55, -0.525, 0),scale_y=0.05,scale_x=3.1,rotation_y=-90, parent=cube),
+    Entity(model='quad', color=color.black, position=(1.55, 0, 0.525),scale_y=3.1,scale_x=0.05,rotation_y=-90, parent=cube),
+    Entity(model='quad', color=color.black, position=(1.55, 0, -0.525),scale_y=3.1,scale_x=0.05,rotation_y=-90, parent=cube)],
+
+
+    [
+    Entity(model='quad', color=color.white, position=(0, 1.55,0), rotation_x=90, parent=cube),
+    Entity(model='quad', color=color.white, position=(1.05, 1.55,0), rotation_x=90, parent=cube),
+    Entity(model='quad', color=color.white, position=(0, 1.55,1.05), rotation_x=90, parent=cube),
+    Entity(model='quad', color=color.white, position=(1.05, 1.55,1.05), rotation_x=90, parent=cube),
+    Entity(model='quad', color=color.white, position=(-1.05, 1.55,0), rotation_x=90, parent=cube),
+    Entity(model='quad', color=color.white, position=(0, 1.55,-1.05), rotation_x=90, parent=cube),
+    Entity(model='quad', color=color.white, position=(-1.05, 1.55,-1.05), rotation_x=90, parent=cube),
+    Entity(model='quad', color=color.white, position=(1.05, 1.55,-1.05), rotation_x=90, parent=cube),
+    Entity(model='quad', color=color.white, position=(-1.05, 1.55,1.05), rotation_x=90, parent=cube),
+
+    Entity(model='quad', color=color.black, position=(0, 1.55, 0.525),scale_y=0.05,scale_x=3.1,rotation_x=90, parent=cube),
+    Entity(model='quad', color=color.black, position=(0, 1.55, -0.525),scale_y=0.05,scale_x=3.1,rotation_x=90, parent=cube),
+    Entity(model='quad', color=color.black, position=(0.525, 1.55, 0),scale_y=3.1,scale_x=0.05,rotation_x=90, parent=cube),
+    Entity(model='quad', color=color.black, position=(-0.525, 1.55, 0),scale_y=3.1,scale_x=0.05,rotation_x=90, parent=cube)],
+
+
+    [
+    Entity(model='quad', color=color.yellow, position=(0,-1.55,0), rotation_x=-90,  parent=cube),
+    Entity(model='quad', color=color.yellow, position=(1.05,-1.55,0), rotation_x=-90,  parent=cube),
+    Entity(model='quad', color=color.yellow, position=(0,-1.55,1.05), rotation_x=-90,  parent=cube),
+    Entity(model='quad', color=color.yellow, position=(1.05,-1.55,1.05), rotation_x=-90,  parent=cube),
+    Entity(model='quad', color=color.yellow, position=(-1.05,-1.55,0), rotation_x=-90,  parent=cube),
+    Entity(model='quad', color=color.yellow, position=(0,-1.55,-1.05), rotation_x=-90,  parent=cube),
+    Entity(model='quad', color=color.yellow, position=(-1.05,-1.55,-1.05), rotation_x=-90,  parent=cube),
+    Entity(model='quad', color=color.yellow, position=(1.05,-1.55,-1.05), rotation_x=-90,  parent=cube),
+    Entity(model='quad', color=color.yellow, position=(-1.05,-1.55,1.05), rotation_x=-90,  parent=cube),
+
+    Entity(model='quad', color=color.black, position=(0, -1.55, 0.525),scale_y=0.05,scale_x=3.1,rotation_x=-90, parent=cube),
+    Entity(model='quad', color=color.black, position=(0, -1.55, -0.525),scale_y=0.05,scale_x=3.1,rotation_x=-90, parent=cube),
+    Entity(model='quad', color=color.black, position=(0.525, -1.55, 0),scale_y=3.1,scale_x=0.05,rotation_x=-90, parent=cube),
+    Entity(model='quad', color=color.black, position=(-0.525, -1.55, 0),scale_y=3.1,scale_x=0.05,rotation_x=-90, parent=cube)]
+
+]
+print(len(faces))"""
+
+rubiks = rubiks_cube(cube,3)
 camera = EditorCamera()  # <-- This gives mouse/scroll movement
 
 app.run()
--- rubiks_cube.py
+++ rubiks_cube.py
@@ -1,32 +1,109 @@
 import enum
+from ursina import *
 
 
-class Color(enum.Enum):
-    WHITE=0
-    RED=1
-    BLUE=2
-    GREEN=3
-    YELLOW=4
-    ORANGE=5
+"""class Color(enum.Enum):
+    #WHITE=0 # UP
+    color.white=0 # UP
+    RED=1 # FORWARD
+    GREED=2 # LEFT
+    ORANGE=3 # BACK
+    BLUE=4 #RIGHT
+    YELLOW=5 #DOWN"""
 
 class rubiks_cube():
 
-
-    def __init__(self,radius):
+    def __init__(self,cube,radius):
         cubes = [[[None for _ in range(radius)] for _ in range(radius)] for _ in range(radius)]
-
-        for x in range(radius):
+        """for x in range(radius):
             for y in range(radius):
                 for z in range(radius):
-                    print
+                    cubes[x][y][z]= cube_piece(cube,(x-1,y-1,z-1),{1:Color.BLUE})
+                    print"""
 
-        print()
+        # All center pieces
+        cubes[1][2][1]= cube_piece(cube,(0,1,0),{0:color.white})
+        cubes[1][1][0]= cube_piece(cube,(0,0,-1),{1:color.red})
+        cubes[0][1][1]= cube_piece(cube,(-1,0,0),{2:color.green})
+        cubes[1][1][2]= cube_piece(cube,(0,0,1),{3:color.orange})
+        cubes[2][1][1]= cube_piece(cube,(1,0,0),{4:color.blue})
+        cubes[1][0][1]= cube_piece(cube,(0,-1,0),{5:color.yellow})
+
+
+        # All border pieces
+        cubes[1][2][0]= cube_piece(cube,(0,1,-1),{0:color.white,1:color.red})
+        cubes[0][2][1]= cube_piece(cube,(-1,1,0),{0:color.white,2:color.green})
+        cubes[1][2][2]= cube_piece(cube,(0,1,1),{0:color.white,3:color.orange})
+        cubes[2][2][1]= cube_piece(cube,(1,1,0),{0:color.white,4:color.blue})
+
+
+        cubes[1][0][0]= cube_piece(cube,(0,-1,-1),{5:color.yellow,1:color.red})
+        cubes[0][0][1]= cube_piece(cube,(-1,-1,0),{5:color.yellow,2:color.green})
+        cubes[1][0][2]= cube_piece(cube,(0,-1,1),{5:color.yellow,3:color.orange})
+        cubes[2][0][1]= cube_piece(cube,(1,-1,0),{5:color.yellow,4:color.blue})
+
+
+        cubes[0][1][0]= cube_piece(cube,(-1,0,-1),{2:color.green,1:color.red})
+        cubes[0][1][2]= cube_piece(cube,(-1,0,1),{2:color.green,3:color.orange})
+        cubes[2][1][2]= cube_piece(cube,(1,0,1),{3:color.orange,4:color.blue})
+        cubes[2][1][0]= cube_piece(cube,(1,0,-1),{1:color.red,4:color.blue})
+
+
+        # All corner pieces
+        cubes[2][2][0]= cube_piece(cube,(1,1,-1),{0:color.white,1:color.red,4:color.blue})
+        cubes[0][2][0]= cube_piece(cube,(-1,1,-1),{0:color.white,1:color.red,2:color.green})
+        cubes[2][2][2]= cube_piece(cube,(1,1,1),{0:color.white,3:color.orange,4:color.blue})
+        cubes[0][2][2]= cube_piece(cube,(-1,1,1),{0:color.white,3:color.orange,2:color.green})
+
+        cubes[2][0][0]= cube_piece(cube,(1,-1,-1),{5:color.yellow,1:color.red,4:color.blue})
+        cubes[0][0][0]= cube_piece(cube,(-1,-1,-1),{5:color.yellow,1:color.red,2:color.green})
+        cubes[2][0][2]= cube_piece(cube,(1,-1,1),{5:color.yellow,3:color.orange,4:color.blue})
+        cubes[0][0][2]= cube_piece(cube,(-1,-1,1),{5:color.yellow,3:color.orange,2:color.green})
+
+
+        
 
 
 
-class small_cube():
+class cube_piece():
 
-    def __init__(self,color,coordinates):
-        self.up = color
+    def __init__(self,cube,coordinates,sides_with_colors):
         self.coordinates = coordinates  # a vec3 like (x,y,z)
+        self.sides_with_colors=sides_with_colors
+        self.entities=[]
+        for side in sides_with_colors:
+            self.entities.append(create_entity(cube,coordinates,side,sides_with_colors[side]))
 
+"""class center_piece(cube_piece):
+    def __init__(self,coordinates,color,size_index):
+        self.coordinates = coordinates  # a vec3 like (x,y,z)
+        self.color = color
+        self.size_index = size_index
+
+class border_piece(cube_piece):
+    def __init__(self,coordinates,color,size_index):
+        self.coordinates = coordinates  # a vec3 like (x,y,z)
+        self.color = color
+        self.size_index = size_index
+
+class corner_piece(cube_piece):
+    pass"""
+
+
+
+def create_entity(cube,coordinates,side,color):
+        if side==0:
+            Entity(model='quad', color=color, position=(coordinates[0]*1.05,1.55,coordinates[2]*1.05),rotation_x=90, parent=cube)
+        elif side==1:
+            Entity(model='quad', color=color, position=(coordinates[0]*1.05,coordinates[1]*1.05,-1.55),rotation_y=0, parent=cube)
+        elif side==2:
+            Entity(model='quad', color=color, position=(-1.55,coordinates[1]*1.05,coordinates[2]*1.05),rotation_y=90, parent=cube)
+        elif side==3:
+            Entity(model='quad', color=color, position=(coordinates[0]*1.05,coordinates[1]*1.05,1.55),rotation_y=180, parent=cube)
+        elif side==4:
+            Entity(model='quad', color=color, position=(1.55,coordinates[1]*1.05,coordinates[2]*1.05),rotation_y=-90, parent=cube)
+        elif side==5:
+            Entity(model='quad', color=color, position=(coordinates[0]*1.05,-1.55,coordinates[2]*1.05),rotation_x=-90, parent=cube)
+        else:
+            print("Entity with no sides")
+            return None
